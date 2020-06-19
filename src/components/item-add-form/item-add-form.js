3b89bcff12fb38730ef2ba92c633490c0c71831e
aleksey.b <skytele@yandex.ru>
2020-06-19 05:54:12 +0300

    add item-add-form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-add-form.css';
 
-const ItemAddForm = ({onAdded}) => {
-  return (
-    <div className="item-add-form">
-      <button type="button" className="btn btn-outline-secondary"
-        onClick={() => onAdded('Sleep')}>      
-        Добавить
-      </button>
-    </div>
-  );
-}
+export default class ItemAddForm extends Component {
 
-export default ItemAddForm;
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value.toUpperCase()
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdded(this.state.label);
+    this.setState(({label}) => {
+      return {
+        label: ''
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type="text"
+                className="form-control"
+                onChange={this.onLabelChange}
+                placeholder="What needs to be done"
+                value={this.state.label}/>
+        <button type="button" className="btn btn-outline-secondary"
+          >      
+          Добавить
+        </button>
+      </form>
+    );
+  }
+}
