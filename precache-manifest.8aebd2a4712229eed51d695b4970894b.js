e5c5a1bb6a5f8eb58e1254d2ba33a35804a6f941
aleksey.b <skytele@yandex.ru>
2020-06-22 12:41:39 +0300

    Updates

---
+++ precache-manifest.8aebd2a4712229eed51d695b4970894b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1159acc688000613269e1c5e426da0ea",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "e715877380b77a26fe50",
+    "url": "/react-first-app/static/css/main.01c4515e.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/react-first-app/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-app/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e715877380b77a26fe50",
+    "url": "/react-first-app/static/js/main.ee419265.chunk.js"
+  },
+  {
+    "revision": "81583e8baf96c3aebb2c",
+    "url": "/react-first-app/static/js/runtime-main.a8ee426c.js"
+  }
+]);
